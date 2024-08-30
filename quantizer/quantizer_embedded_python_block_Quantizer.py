7cea325bbf812b41a2006fecdd1933317b012c1b
Hagtronics <68252415+Hagtronics@users.noreply.github.com>
2024-08-30 10:52:20 -0700

    Update quantizer_embedded_python_block_Quantizer.py

--- quantizer/quantizer_embedded_python_block_Quantizer.py
+++ quantizer/quantizer_embedded_python_block_Quantizer.py
@@ -12,7 +12,7 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
     """Embedded Python Block - Quantizer"""
 
     def __init__(self, bits=255, vref=1.0, clipping=False):  # only default arguments here
-        """ bits (int)      = The number of bits to quantize to
+        """ bits (int)      = The number of bits to quantize input signal to
             vref (float)    = The reference voltage, the range is from: (-vref) to (+vref - 1 LSB)
             clipping (bool) = Flag to specify if the output signal is to be clipped to the
                               range: (-vref) to (+vref - 1 LSB) as would be in a real ADC or DAC.
@@ -25,20 +25,24 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
             out_sig=[np.float32]
         )
 
-        # Calculate variables needed
+        # Save class variables
+        self.bits = bits
         self.vref = vref
-        self.levels = (2**bits)/2
-        self.vmax = vref - (vref / self.levels)
-        self.vmin = -vref
-
-        if not clipping:
-            # If not clipping, then the clip range is +/- Infinity
-            self.vmax = np.finfo(np.float32).max
-            self.vmin = np.finfo(np.float32).min
+        self.clipping = clipping
 
     def work(self, input_items, output_items):
 
+        # These assignments could be out in the __init__(), but then they would not be runtime adjustable.
+        levels = (2**self.bits)/2
+        vmax = self.vref - (self.vref / levels)
+        vmin = -self.vref
+
+        if not self.clipping:
+            # If not clipping, then the clip range is +/- Infinity
+            vmax = np.finfo(np.float32).max
+            vmin = np.finfo(np.float32).min
+
         # The np.floor() function quantizes the signal, the np.clip() function clips the signal to the range specified.
-        output_items[0][:] = np.clip((np.floor(input_items[0]/self.vref*self.levels) / self.levels) * self.vref, a_max=self.vmax, a_min=self.vmin)
+        output_items[0][:] = np.clip((np.floor(input_items[0]/self.vref*levels) / levels) * self.vref, a_max=vmax, a_min=vmin)
 
         return len(output_items[0])
